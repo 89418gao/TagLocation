0e96b4dd71671efd7d691a53d659766ab3ebdf91
Guocui Gao <89418.gao@students.itu.edu>
2016-06-20 14:29:45 -0700

    Initial Commit

---
+++ MyLocations/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  MyLocations
+//
+//  Created by Shuyan Guo on 6/20/16.
+//  Copyright © 2016 GG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ MyLocations/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  MyLocations
+//
+//  Created by Shuyan Guo on 6/20/16.
+//  Copyright © 2016 GG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
